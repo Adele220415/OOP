de143921f9a314cd6ad02e44b2818d8ad3ed26a7
Adele220415 <oluwatumininusolanke@gmail.com>
2025-07-14 22:19:55 +0100

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class BankAccount
+    {
+        //account balance
+        private double accountBalance;
+        //deposit method
+        public void deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                accountBalance += amount;
+                Console.WriteLine($"{amount}: Deposited successfully.\n New Account Balance: {accountBalance}");
+
+            }
+            else
+            {
+                Console.WriteLine("Amount too small for deposit");
+            }
+        }
+        //getter for account balance
+        
+        public double getBalance()
+        {
+            return accountBalance;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account.deposit(14000.00);
+            account.getBalance();
+        }
+    }
+}
